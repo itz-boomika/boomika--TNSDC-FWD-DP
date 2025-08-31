8106c68262ffb570474af318b5a8e8a030e41313
itz-boomika <boomivti2023@gmail.com>
2025-08-31 16:22:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+JavaScript (script.js)
+javascript
+function submitForm(event) {
+  event.preventDefault();
+  document.getElementById("message").textContent = "Message sent successfully!";
+}
+
+  
